dbbadbde566c5ddf12a00de2fe3d6fb1137ad99b
B17055 <e1b17055@oit.ac.jp>
2020-12-22 11:22:33 +0900

    First commit
    MRTKその他アセット導入。インターフェース作成初期段階。

---
+++ Assets/Scripts/MenuObjectCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuObjectCheck : MonoBehaviour
+{
+    bool check = false;
+
+    public void MenuChange()
+    {
+        if (!check)
+        {
+            gameObject.SetActive(true);
+            check = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            check = false;
+        }
+    }
+
+    public void MenuClose()
+    {
+        gameObject.SetActive(false);
+        check = false;
+    }
+}
