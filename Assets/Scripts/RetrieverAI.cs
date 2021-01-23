e690d6900729406283394497a8fe2fcbef2ec426
B17055 <e1b17055@oit.ac.jp>
2021-01-24 01:58:00 +0900

    ランダム移動試作
    アニメーションがうまくいかない

--- Assets/Scripts/RetrieverAI.cs
+++ Assets/Scripts/RetrieverAI.cs
@@ -9,6 +9,10 @@ public class RetrieverAI : MonoBehaviour
     Vector3 animalpos;
     private Animator animator;
     bool idolmode;
+    float idolaction = 100.0f;
+    float actionspan = 5.0f;
+    float currenttime = 0f;
+    Vector3 force;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,7 @@ public class RetrieverAI : MonoBehaviour
         PlaneGenerate();
         animator = GetComponent<Animator>();
         idolmode = true;
+        force = new Vector3(0f, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -27,6 +32,29 @@ public class RetrieverAI : MonoBehaviour
             animalpos.y += 9.5f;
             this.transform.position = animalpos;
             PlaneGenerate();
+        }
+        if(idolmode == true)
+        {
+            Rigidbody rb = this.GetComponent<Rigidbody>();
+            currenttime += Time.deltaTime;
+            if(currenttime > actionspan)
+            {
+                idolaction = Random.Range(1.0f, 100.0f);
+                force = new Vector3(Random.Range(0f, 15.0f) - 7.5f, 0f, Random.Range(0f, 15.0f) - 7.5f);
+                currenttime = 0f;
+            }
+            if(idolaction < 30.0f)
+            {               
+                rb.AddForce(force);
+                animator.SetFloat("Speed", force.magnitude);
+				transform.LookAt(transform.position + force);
+            }
+            else
+            {
+                force = new Vector3(0f, 0f, 0f);
+                rb.AddForce(force);
+                animator.SetFloat("Speed", 0f);
+            }
         }
     }
 
